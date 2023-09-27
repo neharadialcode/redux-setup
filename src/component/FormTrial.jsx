9c3d0baa3137a843ee01cf80bd8253ce289c694c
Neha Rai <neharadialcode@gmail.com>
2023-09-27 12:27:03 +0530

    form validation

--- src/component/FormTrial.jsx
+++ src/component/FormTrial.jsx
@@ -1,16 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FormTrial = () => {
+  const initialValue = {
+    name: "",
+    email: "",
+    address: "",
+    message: "",
+  };
+  const [formData, setFormData] = useState(initialValue);
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log("dfghj");
+  };
   return (
     <div>
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-lg-8">
-            <form action="" className="form_inputes">
-              <input type="text" placeholder="Name" />
-              <input type="email" placeholder="Email" />
-              <input type="text" placeholder="Address" />
-              <input type="text" placeholder="Subject" />
+            <form
+              action=""
+              onSubmit={(e) => onSubmitHandler(e)}
+              className="form_inputes"
+            >
+              <input
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+                type="text"
+                placeholder="Name"
+              />
+              <input
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+                type="email"
+                placeholder="Email"
+              />
+              <input
+                onChange={(e) =>
+                  setFormData({ ...formData, address: e.target.value })
+                }
+                type="text"
+                placeholder="Address"
+              />
+              <input
+                onChange={(e) =>
+                  setFormData({ ...formData, message: e.target.value })
+                }
+                type="text"
+                placeholder="message"
+              />
               <button>Submit</button>
             </form>
           </div>
